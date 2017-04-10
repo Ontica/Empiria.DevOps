cf1f7bd2fb50fc4bf99ada9e8910eb049c9c534d
jmcota <jmcota@ontica.org>
2017-04-10 13:46:14 -0400

    Initial commit

---
+++ DevOps.WebAPI/Empiria.DevOps.WebApi.AssemblyInfo.cs
@@ -0,0 +1,28 @@
+﻿/* Empiria DevOps ********************************************************************************************
+*                                                                                                            *
+*  Solution : Empiria DevOps                                   System  : DevOps Web API                      *
+*  Assembly : Empiria.DevOps.WebApi.dll                        Pattern : Assembly Attributes File            *
+*  Type     : CallerConfiguration                              License : Please read LICENSE.txt file        *
+*                                                                                                            *
+*  Summary  : Web Api that provides DevOps services and artifacts for Empiria-based applications.            *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+/*************************************************************************************************************
+* Assembly configuration attributes.                                                                         *
+*************************************************************************************************************/
+[assembly: AssemblyTitle("Empiria.DevOps.WebApi")]
+[assembly: AssemblyDescription("Web Api that provides DevOps services and artifacts for Empiria-based applications.")]
+[assembly: AssemblyVersion("1.0.0625.0")]
+[assembly: AssemblyFileVersion("1.0.0625")]
+[assembly: AssemblyCompany("Ontica")]
+[assembly: AssemblyProduct("Empiria DevOps")]
+[assembly: AssemblyCopyright("(c) 2016-2017. La Vía Óntica SC, Ontica LLC and contributors.")]
+[assembly: AssemblyTrademark("Empiria and Ontica are either trademarks of La Vía Óntica SC or Ontica LLC.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
---
+++ README.md
@@ -0,0 +1,27 @@
+﻿# About Empiria DevOps
+
+**Empiria DevOps** is a software configuration management solution
+used to provide software artifacts and services or Empiria-based applications deployments.
+
+As other Empiria products, this backend runs over [Empiria Framework](https://github.com/Ontica/Empiria.Core)
+components and, as usual, also needs some of the [Empiria Extensions](https://github.com/Ontica/Empiria.Extensions).
+
+The source code can be compiled using Visual Studio Community Edition.
+
+# Contents
+
+For now, Empiria DevOps only includes the following C# project:
+
+**Web API**
+
+Http/Json RESTful type web services used to communicate with the solution backend.
+
+
+# License
+
+This system is distributed by the GNU AFFERO GENERAL PUBLIC LICENSE.
+
+
+# Copyright
+
+Copyright © 2017. La Vía Óntica SC, Ontica LLC and colaborators.
