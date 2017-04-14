485b399442a07bc5659829fa358f5187001195d9
jmcota <jmcota@ontica.org>
2017-04-14 13:53:59 -0400

    ProtectString WebApi method added

---
+++ DevOps.WebAPI/Controllers/GeneralMethodsController.cs
@@ -0,0 +1,60 @@
+﻿/* Empiria DevOps ********************************************************************************************
+*                                                                                                            *
+*  Solution : Empiria DevOps                                   System  : DevOps Web API                      *
+*  Assembly : Empiria.DevOps.WebApi.dll                        Pattern : Web Api Controller                  *
+*  Type     : GeneralMethodsController                         License : Please read LICENSE.txt file        *
+*                                                                                                            *
+*  Summary  : General purpose web methods for the DevOps Web Api.                                            *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+using System.Net.Http;
+using System.Web.Http;
+
+using Empiria.WebApi;
+using Empiria.WebApi.Models;
+
+namespace Empiria.DevOps.WebApi {
+
+  /// <summary>General purpose web methods for the DevOps Web Api.</summary>
+  public class GeneralMethodsController : WebApiController {
+
+    #region Public APIs
+
+    /// <summary>Gets a protected string encrypted according to the caller rules.</summary>
+    /// <param name="value">The string to be encrypted.</param>
+    /// <remarks>An Empiria-DevOps-DeploymentID request header must be supplied.</remarks>
+    [HttpPost, AllowAnonymous]
+    [Route("v1/general/protect-string")]
+    public SingleObjectModel ProtectString([FromBody] string plainText) {
+      try {
+        base.RequireBody(plainText);
+
+        Deployment deployment = GetDeployment(this.Request);
+
+        var cryptographer = new DevOpsCryptographer(deployment);
+
+        string encryptedValue = cryptographer.Encrypt(plainText);
+
+        return new SingleObjectModel(this.Request, encryptedValue);
+
+      } catch (Exception e) {
+        throw base.CreateHttpException(e);
+      }
+    }
+
+    #endregion Public APIs
+
+    #region Private methods
+
+    private Deployment GetDeployment(HttpRequestMessage request) {
+      DeploymentID deploymentID = base.GetRequestHeader<DeploymentID>(DeploymentID.RequestHeaderName);
+
+      return Deployment.Parse(deploymentID);
+    }
+
+    #endregion Private methods
+
+  }  // class GeneralMethodsController
+
+}  // namespace Empiria.DevOps.WebApi
