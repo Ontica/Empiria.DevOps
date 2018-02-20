b725d2d41db7dae561e9c744e6b7a35d15ba3303
jmcota <jmcota@ontica.org>
2018-02-20 15:40:42 -0500

    Migrate to VS 2017 .csproj format

--- DevOps.WebAPI/Empiria.DevOps.WebApi.AssemblyInfo.cs
+++ DevOps.WebAPI/Empiria.DevOps.WebApi.AssemblyInfo.cs
@@ -1,28 +1,18 @@
 ﻿/* Empiria DevOps ********************************************************************************************
 *                                                                                                            *
-*  Solution : Empiria DevOps                                   System  : DevOps Web API                      *
+*  Solution : Empiria DevOps                                   System  : Web Api interface                   *
 *  Assembly : Empiria.DevOps.WebApi.dll                        Pattern : Assembly Attributes File            *
 *                                                              License : Please read LICENSE.txt file        *
 *                                                                                                            *
-*  Summary  : Web Api that provides DevOps services and artifacts for Empiria-based applications.            *
+*  Summary  : Web API HTTP/JSON RESTful interface to access Empiria DevOps services.                         *
 *                                                                                                            *
 ************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
 using System;
 using System.Reflection;
-using System.Runtime.InteropServices;
 
 /*************************************************************************************************************
 * Assembly configuration attributes.                                                                         *
 *************************************************************************************************************/
-[assembly: AssemblyTitle("Empiria.DevOps.WebApi")]
-[assembly: AssemblyDescription("Web Api that provides DevOps services and artifacts for Empiria-based applications.")]
-[assembly: AssemblyVersion("1.0.0625.0")]
-[assembly: AssemblyFileVersion("1.0.0625")]
-[assembly: AssemblyCompany("Ontica")]
-[assembly: AssemblyProduct("Empiria DevOps")]
-[assembly: AssemblyCopyright("(c) 2016-2017. La Vía Óntica SC, Ontica LLC and contributors.")]
 [assembly: AssemblyTrademark("Empiria and Ontica are either trademarks of La Vía Óntica SC or Ontica LLC.")]
-[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCulture("")]
-[assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
---
+++ DevOps.WebAPI/README.md
@@ -0,0 +1,11 @@
+﻿# Empiria DevOps Web Api
+
+Web API HTTP/JSON RESTful interface to access Empiria DevOps services.
+
+# License
+
+This system is distributed by the GNU AFFERO GENERAL PUBLIC LICENSE.
+
+# Copyright
+
+Copyright © 2016-2018. La Vía Óntica SC, Ontica LLC and colaborators.
